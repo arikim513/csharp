cfd86b1ab07527049a0a54ec59f63a0f840eb095
Ari Kim <ari.kim@avanade.com>
2020-01-12 15:20:12 +0900

    First Commit HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("How to use : HelloWorld.exe <name>");
+                return;
+            }
+
+            WriteLine("Hello World! {0}", args[0]);
+        }
+    }
+}
